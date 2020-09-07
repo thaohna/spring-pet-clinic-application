dd6204e806e057c597e8b254afd2a6ef2e41054d
Thao <huynhngoanhthao@gmail.com>
2020-09-08 04:02:07 +0700

    Initial Project Add

---
+++ src/main/java/com/anhthao/petclinic/AnhthaoPetClinicApplication.java
@@ -0,0 +1,13 @@
+package com.anhthao.petclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnhthaoPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnhthaoPetClinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/anhthao/petclinic/AnhthaoPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anhthao.petclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnhthaoPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
